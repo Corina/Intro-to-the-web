a5896b6db1a664034bb2badd50c1a97663e596ca
Corina <fluturelcorina@gmail.com>
2017-06-20 10:48:31 +0100

    Adding forms

--- Intro to the web/Sinatra/app.rb
+++ Intro to the web/Sinatra/app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
-  get '/cat' do
-    '<img style="border: 5px dashed red" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png" alt="cat">'
+  get '/random-cat' do
+    @name = ["Amigo", "Oscar", "Viking"].sample
+    erb(:index)
+  end
+
+  get '/named-cat' do
+    p params
+    @name = params[:name]
+    erb(:index)
   end
